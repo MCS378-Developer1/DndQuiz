3401fe4373397983ec85bf0eae0b4a053f10d335
mikekebede <mikyasyesigat@gmail.com>
2024-03-19 10:14:41 -0500

    "add commit"

--- app/src/main/java/com/example/dndquiz/MainActivity.kt
+++ app/src/main/java/com/example/dndquiz/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
 
     // create cheatLauncher
-    //random comment
+
 
     private val cheatLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
